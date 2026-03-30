b0f44644d7f1511dbd25dfd727055a61b4cef943
Ralph Seichter <ralph@seichter.de>
2026-03-30 22:52:54 +0200

    Updates

--- package.nix
+++ package.nix
@@ -1,18 +1,27 @@
 {
   lib,
   buildGoModule,
+  fetchFromGitHub,
 }:
+let
+  myrev = "6c935bb9eec7757001a878fa129f4173acb6ff04";
+  myhash = "sha256-b7ZqkYmOIWiV4Q/Kc6I9R2FnsI8TZ2wIddmdO/wfVf4=";
+  # myhash = lib.fakeHash;
+in
 buildGoModule (finalAttrs: {
   pname = "scare";
   version = "0.6.dev1";
 
-  src = ./.;
-  # src = fetchFromGitHub {
-  #   owner = "rseichter";
-  #   repo = "scare";
-  #   tag = "v${finalAttrs.version}";
-  #   hash = lib.fakeHash;
-  # };
+  # src = ./.;
+
+  src = fetchFromGitHub {
+    owner = "rseichter";
+    repo = "scare";
+    hash = myhash;
+    rev = myrev;
+    # tag = "v${finalAttrs.version}";
+  };
+
   vendorHash = null;
   env.CGO_ENABLED = 0;
 
